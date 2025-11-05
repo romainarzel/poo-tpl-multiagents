b3f88101e2746607bdf6d97906680bfbafe562bb
BOUCHUT Tanguy <tanguy.bouchut@phelma.grenoble-inp.fr>
2025-11-05 19:19:04 +0100

    FEAT ajout des rebonds contre les parois

--- src/TestBalls.java
+++ src/TestBalls.java
@@ -1,8 +1,8 @@
 import java.awt.Color;
 import java.awt.Point;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+//import java.util.Iterator;
+//import java.util.List;
 
 import gui.*;
 
@@ -14,59 +14,68 @@ public class TestBalls {
 }
 
 class Balls{
-    private int ballsRadius;
+    private final int ballsRadius;
     private ArrayList<Point> points;
+    private ArrayList<Integer> dx;
+    private ArrayList<Integer> dy;
 
     public Balls(int ballsRadius){
         this.ballsRadius = ballsRadius;
         reInit();
     }
-
+    //Accesseurs
     public int getRadius(){
         return this.ballsRadius;
     }
-
     public ArrayList<Point> getPoints(){
         return this.points;
     }
+    public ArrayList<Integer> getdx(){
+        return this.dx;
+    }
+    public ArrayList<Integer> getdy(){
+        return this.dy;
+    }
 
     public void reInit(){
-        points = new ArrayList<Point>();
+        points = new ArrayList<>();
+        dx = new ArrayList<>();
+        dy = new ArrayList<>();
         for (int x = 10; x < 80; x += 50){
             for (int y = 10; y < 60; y += 50){
                 points.add(new Point(x, y));
+                dx.add(1);
+                dy.add(1);
             }
         }
     }
 
-    public void translate(int dx, int dy){
-        for(Point p : points){
-            p.translate(dx, dy);
+    public void translate(){
+        for(int i=0; i<points.size(); i++){
+            points.get(i).translate(dx.get(i), dy.get(i));
         }
     }
 
     @Override
     public String toString() {
-        String str = "position des balles : \n";
+        StringBuilder str = new StringBuilder("position des balles : \n");
         for (Point p : points){
-            str += p.toString() + "\n";
+            str.append(p.toString()).append("\n");
         }
-        return str;
+        return str.toString();
     }
 }
 
 class BallsSimulator extends Balls implements Simulable{
-    private GUISimulator gui;
-    private Color ballsColor;
-    private Iterator<Integer> dxIterator;
-    private Iterator<Integer> dyIterator;
+    private final GUISimulator gui;
+    private final Color ballsColor;
 
     public BallsSimulator(GUISimulator gui, Color ballsColor, int ballsRadius){
         super(ballsRadius);
         this.gui = gui;
         gui.setSimulable(this);
         this.ballsColor = ballsColor;
-        System.out.println(this.toString()); //Q.2 Affiche simplement l'état des balles sans graphismes
+        System.out.println(this); //Q.2 Affiche simplement l'état des balles sans graphismes
         draw();
     }
 
@@ -77,21 +86,27 @@ class BallsSimulator extends Balls implements Simulable{
         }
     }
 
-    public void next(){
-        int dx = 1;
-        int dy = 1;
+    public void verifTranslation(){
+        for(int i=0; i<getPoints().size(); i++){
+            if(getPoints().get(i).getX() <= getRadius() || getPoints().get(i).getX() >= gui.getWidth()-getRadius()){
+                getdx().set(i, -getdx().get(i));
+            }
+            if(getPoints().get(i).getY() <= getRadius()-5 || getPoints().get(i).getY() >= gui.getHeight()-getRadius()-84){
+                getdy().set(i, -getdy().get(i));
+            }
+        }
+    }
 
-        this.translate(dx, dy);
-        System.out.println(this.toString()); //Q.2 Affiche simplement l'état des balles sans graphismes
+    public void next(){
+        this.translate();
+        verifTranslation();
+        System.out.println(this); //Q.2 Affiche simplement l'état des balles sans graphismes
         draw(); //Q.3 Affiche graphiquement
     }
 
     public void restart(){
-        //planCoordinates();
-        //draw();
-
         this.reInit();
-        System.out.println(this.toString()); //Q.2 Affiche simplement l'état des balles sans graphismes
+        System.out.println(this); //Q.2 Affiche simplement l'état des balles sans graphismes
         draw(); //Q.3 Affiche graphiquement
     }
 
